ea658433af1e6c0a65e59f88cddd6c8c38ba80be
David Tolliver <davidtolliver1@gmail.com>
2023-01-19 05:55:39 -0500

    Second commit

--- Blackjack_21.py
+++ Blackjack_21.py
@@ -1,63 +1,118 @@
 import random
+import sys
 
-class Card:
-    def __init__(self, value, suit):
-        self.value = value
+# ♥♠️♦️♣️
+number_values = [("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("10", 10), ("Jack", 10), ("Queen", 10), ("King", 10), ("Ace", 1)]
+
+class Card():
+    def __init__(self, suit, number, value):
         self.suit = suit
+        self.number = number
+        self.value = value
+    def __repr__(self):
+        return "{number} of {suit}s".format(number = self.number, suit = self.suit)
 
-class Deck:
+
+class Deck():
     def __init__(self):
-        self.cards = [Card(value, suit) for value in range(2, 11) for suit in ["hearts", "diamonds", "clubs", "spades"]]
-
+      self.cards = [Card(suit, number, value) for suit in ["♥", "♠️", "♦️", "♣️"] for number, value in number_values]
     def shuffle(self):
         random.shuffle(self.cards)
-
     def draw(self):
         return self.cards.pop()
 
-class Player:
+class Player():
     def __init__(self, name):
         self.name = name
         self.hand = []
 
     def score(self):
-        return sum(card.value for card in self.hand)
+      score = 0 + sum(card.value for card in self.hand)
+      return score
 
-class Blackjack:
+class Blackjack():
     def __init__(self):
         self.deck = Deck()
-        self.player = Player("Player")
+        self.player = Player(input("Please Input A Name..."))
         self.dealer = Player("Dealer")
 
     def play(self):
         self.deck.shuffle()
         self.player.hand.append(self.deck.draw())
         self.player.hand.append(self.deck.draw())
+        print("You have {value} | {hand}".format(hand = self.player.hand, value = sum(card.value for card in self.player.hand)))
+
         self.dealer.hand.append(self.deck.draw())
         self.dealer.hand.append(self.deck.draw())
+        print("Dealer showing {hand}".format(hand = self.dealer.hand[0]))
 
         while True:
-            choice = input(f"{self.player.name}, do you want to hit or stay? ").lower()
-            if choice == "hit":
-                self.player.hand.append(self.deck.draw())
-                if self.player.score() > 21:
-                    print("You bust!")
-                    break
-            elif choice == "stay":
+
+            ace_card = next((card for card in self.player.hand if card.number == "Ace"), None)
+            if self.player.score() <=  11 and ace_card:
+              choice = input("You drew an Ace! Would you like to count it as 1 or 11.")
+              if choice == "11":
+                ace_card.value = 11
+                print("You have {value} | {hand}".format(hand = self.player.hand, value = sum(card.value for card in self.player.hand)))
+              if choice not in ["1", "11"]:
+                print("Invalid Input, please enter again")
+                choice = input("You drew an Ace! Would you like to count it as 1 or 11...")
+
+              elif choice == "1":
+                pass
+            if self.player.score() == 21:
                 break
 
+            choice = input(f"{self.player.name}, do you want to hit or stay? ")
+              
+            if choice == "hit":
+                self.player.hand.append(self.deck.draw())
+                
+            print("You have {value} | {hand}".format(hand = self.player.hand, value = sum(card.value for card in self.player.hand)))
+                
+            if self.player.score() > 21:
+                    print("You bust!")
+                    print("Dealer Wins!")
+                    return
+            elif choice == "stay":
+                break
+          
+        ace_card = next((card for card in self.dealer.hand if card.number == "Ace"), None)
+
+        if self.dealer.score() <= 11 and ace_card:
+            ace_card.value = 11
+        
         while self.dealer.score() < 17:
             self.dealer.hand.append(self.deck.draw())
             if self.dealer.score() > 21:
+                print("Dealer had {value} | {hand}".format(value = sum(card.value for card in self.dealer.hand), hand = self.dealer.hand))
                 print("Dealer bust!")
-                break
+                print("You win!")
+                return
 
-        if self.player.score() > self.dealer.score() or self.dealer.score() > 21:
+        if (self.player.score() > self.dealer.score() and self.player.score() <= 21) or self.dealer.score() > 21:
+            print("Dealer had {value} | {hand}".format(value = sum(card.value for card in self.dealer.hand), hand = self.dealer.hand))
             print("You win!")
         elif self.player.score() < self.dealer.score():
+            print("Dealer had {value} | {hand}".format(value = sum(card.value for card in self.dealer.hand), hand = self.dealer.hand))
             print("You lose!")
         else:
+            print("Dealer had {value} | {hand}".format(value = sum(card.value for card in self.dealer.hand), hand = self.dealer.hand))
             print("It's a tie!")
 
-game = Blackjack()
-game.play()
+def play_game():
+    game = Blackjack()
+    game.play()
+    again = input("Do you want to play again? (yes/no)")
+    if again == "yes":
+        return
+    if again == "no":
+      sys.exit()
+    if again not in ["yes", "no"]:
+      print("Invalid Input, please enter again")
+      again = input("Do you want to play again? (yes/no)")
+    
+
+while True:
+    play_game()
+
